e48a6c8f7f5ceffd605e73b4f37c968180d9e49d
axelman03 <axelman03@gmail.com>
2017-01-21 16:07:15 -0500

    Wow, i removed a button

--- app/src/main/java/com/example/malcolmmachesky/testapp/ambientSoundsActivity.java
+++ app/src/main/java/com/example/malcolmmachesky/testapp/ambientSoundsActivity.java
@@ -2,11 +2,17 @@ package com.example.malcolmmachesky.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.Manifest.permission;
 
 public class ambientSoundsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+//        <manifest ...>
+//            <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+//                ...
+//        </manifest>
+
         setTitle("Ambient, Alarm and Static Music");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ambient_sounds);
