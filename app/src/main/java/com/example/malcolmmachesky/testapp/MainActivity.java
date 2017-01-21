5d06b2ca217bd2b0bb057a826844e0f3c47b8f3a
malcolmmachesky <malcolm9292@gmail.com>
2017-01-21 04:58:09 -0500

    settings app swich impletation complete

--- app/src/main/java/com/example/malcolmmachesky/testapp/MainActivity.java
+++ app/src/main/java/com/example/malcolmmachesky/testapp/MainActivity.java
@@ -3,15 +3,30 @@ package com.example.malcolmmachesky.testapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     String title = "SuperSleep";
+    public final static String MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public final static String MESSAGEALERT = "com.example.myfirstapp.MESSAGEALERT";
+    public final static String MESSAGESNOOZE = "com.example.myfirstapp.MESSAGESNOOZE";
+    public final static String MESSAGECANTSTOP = "com.example.myfirstapp.MESSAGECANTSTOP";
+    boolean soothing = false;
+    boolean minuteAlert = false;
+    boolean snooze = false;
+    boolean cantStop = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTitle(title);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        soothing = intent.getBooleanExtra(settingsActivity.MESSAGE,false);
+        minuteAlert = intent.getBooleanExtra(settingsActivity.MESSAGEALERT, false);
+        snooze = intent.getBooleanExtra(settingsActivity.MESSAGESNOOZE, false);
+        cantStop = intent.getBooleanExtra(settingsActivity.MESSAGECANTSTOP, false);
+
     }
 
     public void openAlarmSet(View view){
@@ -26,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void openSettings(View view){
         Intent intent = new Intent(this, settingsActivity.class);
+        intent.putExtra(MESSAGE, soothing);
+        intent.putExtra(MESSAGEALERT, minuteAlert);
+        intent.putExtra(MESSAGESNOOZE, snooze);
+        intent.putExtra(MESSAGECANTSTOP, cantStop);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/malcolmmachesky/testapp/settingsActivity.java
+++ app/src/main/java/com/example/malcolmmachesky/testapp/settingsActivity.java
@@ -1,14 +1,102 @@
 package com.example.malcolmmachesky.testapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Switch;
 
 public class settingsActivity extends AppCompatActivity {
+    public final static String MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public final static String MESSAGEALERT = "com.example.myfirstapp.MESSAGEALERT";
+    public final static String MESSAGESNOOZE = "com.example.myfirstapp.MESSAGESNOOZE";
+    public final static String MESSAGECANTSTOP = "com.example.myfirstapp.MESSAGECANTSTOP";
 
+
+    boolean soothing = false;
+    boolean minuteAlert = false;
+    boolean snooze = false;
+    boolean cantStop = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTitle("Settings");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Intent intent = getIntent();
+        soothing = intent.getBooleanExtra(MainActivity.MESSAGE,false);
+        minuteAlert = intent.getBooleanExtra(MainActivity.MESSAGEALERT, false);
+        snooze = intent.getBooleanExtra(MainActivity.MESSAGESNOOZE, false);
+        cantStop = intent.getBooleanExtra(MainActivity.MESSAGECANTSTOP, false);
+        if(soothing){
+            ((Switch) findViewById(R.id.SoothingNoise)).setChecked(true);
+        }
+        else {
+            ((Switch) findViewById(R.id.SoothingNoise)).setChecked(false);
+        }
+        if(minuteAlert){
+            ((Switch) findViewById(R.id.MinuteAlert)).setChecked(true);
+        }
+        else{
+            ((Switch) findViewById(R.id.MinuteAlert)).setChecked(false);
+        }
+        if(snooze){
+            ((Switch) findViewById(R.id.Snooze)).setChecked(true);
+        }
+        else{
+            ((Switch) findViewById(R.id.Snooze)).setChecked(false);
+        }
+        if(cantStop){
+            ((Switch) findViewById(R.id.CantStop)).setChecked(true);
+        }else{
+            ((Switch) findViewById(R.id.CantStop)).setChecked(false);
+        }
     }
+
+
+
+    public void onCheck(View view){
+        boolean checked = ((Switch) view).isChecked();
+        switch(view.getId()){
+            case R.id.SoothingNoise:
+                if (checked){
+                    soothing = true;
+                }else{
+                    soothing = false;
+                }
+                break;
+            case R.id.MinuteAlert:
+                if(checked){
+                    minuteAlert = true;
+                }else{
+                    minuteAlert = false;
+                }
+                break;
+            case R.id.Snooze:
+                if(checked){
+                    snooze = true;
+                }else{
+                    snooze = false;
+                }
+                break;
+            case R.id.CantStop:
+                if(checked){
+                    cantStop = true;
+                }else{
+                    cantStop = false;
+                }
+                break;
+        }
+    }
+
+    public void backToMenu(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra(MESSAGE, soothing);
+        intent.putExtra(MESSAGEALERT, minuteAlert);
+        intent.putExtra(MESSAGESNOOZE, snooze);
+        intent.putExtra(MESSAGECANTSTOP, cantStop);
+        startActivity(intent);
+
+    }
+
 }
