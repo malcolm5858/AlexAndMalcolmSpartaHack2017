72a152156034d0c46a3f95c89fccd7316d270c18
malcolmmachesky <malcolm9292@gmail.com>
2017-01-21 01:44:34 -0500

    Base for the app

--- app/src/main/java/com/example/malcolmmachesky/testapp/MainActivity.java
+++ app/src/main/java/com/example/malcolmmachesky/testapp/MainActivity.java
@@ -3,11 +3,12 @@ package com.example.malcolmmachesky.testapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class FirstActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity {
+    String title = "SuperSleep";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTitle(title);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
+        setContentView(R.layout.activity_main);
     }
 }
