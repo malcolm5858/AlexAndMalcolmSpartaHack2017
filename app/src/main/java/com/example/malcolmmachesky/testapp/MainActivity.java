1976f7c9a0ab4a0414fd5a853d0058bf4b07eee2
malcolmmachesky <malcolm9292@gmail.com>
2017-01-21 01:52:39 -0500

    all the buttons work

--- app/src/main/java/com/example/malcolmmachesky/testapp/MainActivity.java
+++ app/src/main/java/com/example/malcolmmachesky/testapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.malcolmmachesky.testapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     String title = "SuperSleep";
@@ -11,4 +13,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openAlarmSet(View view){
+        Intent intent = new Intent(this, alarmSetActivity.class);
+        startActivity(intent);
+    }
+
+    public void openAmbientSounds(View view){
+        Intent intent = new Intent(this, ambientSoundsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openSettings(View view){
+        Intent intent = new Intent(this, settingsActivity.class);
+        startActivity(intent);
+    }
+
 }
